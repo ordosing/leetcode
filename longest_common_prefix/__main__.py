e626312eb312b205334bd6a0e2e410e7580e473d
erik.ordos <erikordos@gmail.com>
2022-11-28 22:36:48 -0500

    Working solution

--- longest_common_prefix/__main__.py
+++ longest_common_prefix/__main__.py
@@ -28,14 +28,37 @@ from typing import List
 
 class Solution():
     def longestCommonPrefix(strs: List[str]) -> str:
-        prefix_map = {}
+        list_len = len(strs)
+        # If the list is 1 element, return
+        if list_len == 1:
+            return strs[0]
+        shortest_string = min(strs, key=len)
+        common = ''
+        # Set outer loop counter
         i = 0
-        for x in range(len(strs)):
-            prefix_map[x] = strs[x][i]
-        i + 1
-        print(prefix_map)
+        # Only iterate over the length of the shortest string
+        while i < len(shortest_string):
+            # Get the letter of in position i of the first string in list
+            letter = strs[0][i]
+            # Set inner loop counter
+            x = 1
+            while x < list_len:
+                # Get the letter in the string
+                compare = strs[x][i]
+                # Compare the letter with the same index of the previous string
+                if compare != letter:
+                    return common
+                x = x + 1
+            common += letter
+            i = i + 1
+        return common
 
 
 if __name__ == '__main__':
     solution_class = Solution
-    solution_class = Solution.longestCommonPrefix(["flower", "flow", "flight"])
+    solution = Solution.longestCommonPrefix(["flower", "flow", "flight"])
+    print(solution)
+    solution = Solution.longestCommonPrefix(["dog", "racecar", "car"])
+    print(solution)
+    solution = Solution.longestCommonPrefix(["sheeple", "sheeps", "sheeping"])
+    print(solution)
